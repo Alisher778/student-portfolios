673748e1db2dee37b160e910d19d6db1993cd310
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-01-11 12:36:02 +0500

    task done

--- abdumalik/main.js
+++ abdumalik/main.js
@@ -3,20 +3,26 @@ $(document).ready(function() {
         e.preventDefault();
         $('#biograf').css('display', 'block')
     });*/
+    $(document).scroll(function() {
+    	var scrollPosition = $(this).scrollTop();
+    	var info = $('#info').offset().top;
+
+    	/*if(scrollPosition >= info) {
+    		$('#info').css('position', 'fixed')
+    		console.log('scroll')
+    	}*/
+    	
+    })
     $('.into').slick({
-        slidesToShow: 8,
+        slidesToShow: 6,
         slidesToScroll: 1,
-        
-  		focusOnSelect: true,
-        
+        focusOnSelect: true,
         asNavFor: '#area'
     });
     $('#area').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         asNavFor: '.into',
-        
-        
         focusOnSelect: true
     });
     $('.img').slick({
@@ -24,29 +30,30 @@ $(document).ready(function() {
         slidesToShow: 3,
         slidesToScroll: 1,
         speed: 500,
-        /*cssEase: 'linear',
         autoplay: true,
-        autoplaySpeed: 2000,
-        centerMode: true,
-        centerPadding: '220px',
-        slidesToShow: 3,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '200px',
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '200px',
-                slidesToShow: 1
-            }
-        }]*/
+        autoplaySpeed: 4000,
+        cssEase: 'linear',
+        /*
+         centerMode: true,
+         centerPadding: '220px',
+         slidesToShow: 3,
+         responsive: [{
+             breakpoint: 768,
+             settings: {
+                 arrows: false,
+                 centerMode: true,
+                 centerPadding: '200px',
+                 slidesToShow: 3
+             }
+         }, {
+             breakpoint: 480,
+             settings: {
+                 arrows: false,
+                 centerMode: true,
+                 centerPadding: '200px',
+                 slidesToShow: 1
+             }
+         }]*/
     })
     $('.tarif').slick({
         dots: true,
@@ -63,6 +70,19 @@ $(document).ready(function() {
     		$('#info').css('position', 'fixed');
     	}
     })*/
+    $('.into button').click(function() {
+    	$(this).addClass('active');
+    	console.log('actived')
+    })
+    $('#hire').click(function(e) {
+       $('#mail').css('position', 'absolute');
+       console.log('ishladi');
+   })
+    $('#mail').mouseleave(function(e) {
+    	$(this).css('position', 'relative');
+    	
+    })
+
 })
 //#423b3b
 //#10100f
