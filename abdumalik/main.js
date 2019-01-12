dfa1fa8352d4c411922a978b0c2868f102152eff
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-01-12 15:18:43 +0500

    task-2

--- abdumalik/main.js
+++ abdumalik/main.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-    /*$('#bio').click(function(e) {
-        e.preventDefault();
-        $('#biograf').css('display', 'block')
-    });*/
-    $(document).scroll(function() {
-    	var scrollPosition = $(this).scrollTop();
-    	var info = $('#info').offset().top;
-
-    	/*if(scrollPosition >= info) {
-    		$('#info').css('position', 'fixed')
-    		console.log('scroll')
-    	}*/
-    	
-    })
     $('.into').slick({
         slidesToShow: 6,
         slidesToScroll: 1,
@@ -33,56 +19,25 @@ $(document).ready(function() {
         autoplay: true,
         autoplaySpeed: 4000,
         cssEase: 'linear',
-        /*
-         centerMode: true,
-         centerPadding: '220px',
-         slidesToShow: 3,
-         responsive: [{
-             breakpoint: 768,
-             settings: {
-                 arrows: false,
-                 centerMode: true,
-                 centerPadding: '200px',
-                 slidesToShow: 3
-             }
-         }, {
-             breakpoint: 480,
-             settings: {
-                 arrows: false,
-                 centerMode: true,
-                 centerPadding: '200px',
-                 slidesToShow: 1
-             }
-         }]*/
     })
     $('.tarif').slick({
-        dots: true,
+        //dots: true,
         infinite: true,
         speed: 500,
         fade: true,
         cssEase: 'linear'
     })
-    /*$(document).scroll(function() {
-
-    	var scrollPosition = $('#info').offset().top;
-		$(this).scrollTop = scrollPosition; 
-    	if(scrollPosition) {
-    		$('#info').css('position', 'fixed');
-    	}
-    })*/
     $('.into button').click(function() {
-    	$(this).addClass('active');
-    	console.log('actived')
+        $(this).addClass('active');
+        console.log('actived')
     })
-    $('#hire').click(function(e) {
-       $('#mail').css('position', 'absolute');
-       console.log('ishladi');
-   })
+    /*$('#hire').click(function(e) {
+        $('#mail').css('position', 'fixed');
+        console.log('ishladi');
+    })*/
     $('#mail').mouseleave(function(e) {
-    	$(this).css('position', 'relative');
-    	
+        $(this).css('position', 'relative');
     })
-
 })
 //#423b3b
 //#10100f
