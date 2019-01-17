ee8eaad9fb1609f7765688779dc87477ee5bf0f1
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-01-17 14:57:59 +0500

    responsive holati tayyor, areada kamchilik bor

--- abdumalik/main.js
+++ abdumalik/main.js
@@ -93,10 +93,11 @@ $(document).ready(function() {
         slidesToScroll: 1,
         speed: 500,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
         cssEase: 'linear',
     })
-    $('.tarif').slick({       
+    $('.tarif').slick({ 
+              
         infinite: true,
         speed: 500,
         fade: true,
